d72f171031867f21c20e789044a988d982bdbf0d
Ebtsam <ebtsam.negash@gmail.com>
2022-07-19 09:59:49 -0400

    files added

---
+++ Test/com/generation/service/CourseServiceTest.java
@@ -0,0 +1,30 @@
+package com.generation.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourseServiceTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void registerCourse() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getCourse() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void enrollStudent() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void showEnrolledStudents() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void showSummary() {
+    }
+}
